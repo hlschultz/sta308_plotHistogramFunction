a83eb331c9aa299d4989c2619a6401fd2e9db7c3
hlschultz <schulth4@miamioh.edu>
2022-03-28 11:00:26 -0400

    Added some conditionals for sample size

--- plotRandomFunction.R
+++ plotRandomFunction.R
@@ -4,6 +4,7 @@
 ##
 ## Hannah Schultz
 ## March 18, 2022
+## amended on March 28, 2022 for error checking
 ##
 ## A function that generates a sequence of random normal variables
 ## and makes a histogram.
@@ -21,8 +22,22 @@
 # }
 ## ctrl + shift + C toggles all comments in a selected text block
 ## Today, we will add to it.
+
+## is.wholenumber() is from the help documentation on is.integer
+is.wholenumber <- function(x, tol = .Machine$double.eps^0.5) {
+  abs(x - round(x)) < tol
+}
+
 plotRandom <- function(n=1, mu=0, sigma=1, hist_breaks=6) { ## Change to Greek symbols only to not reuse words
-  hist(rnorm(n=n, mean=mu, sd=sigma))
+  if ( !is.wholenumber(n) ) {
+    warning(paste("Sample size n should be a whole number \n R is using n=floor(n)=", floor(n)))
+  }
+  if(n < 0) {
+    stop("Sample size n must be positive") ## error message changed to Error in plotRandom(n = -20) : Sample size n must be positive
+  } else {
+    hist(rnorm(n=n, mean=mu, sd=sigma),
+         breaks=hist_breaks)
+  }
 }
 ## Note tester fails if mu and sigma are "missing, with no default"
 ## Functions always need default parameters
--- testingPlotFunction.R
+++ testingPlotFunction.R
@@ -27,3 +27,22 @@ hist(x, breaks=1) ## Breaks: how much separation, aka. 2 boxes here
 hist(x, breaks=0) ## Fails because we need at least 2 bins
 hist(x, breaks=3)
 hist(x, breaks=12) ##
+
+## today, march 28, we will update our function to prevent
+## user error.
+plotRandom(n=-20) ## does not run
+plotRandom(n=43.5234) ## runs, but 
+        ## should not because n should be a positive integer
+        ## runs because it rounds up to make the number an integer
+## to understand what is happening...
+x <- rnorm(n=43.5234)
+length(x)
+## ^ appears to be truncating, = 43
+## we can do two things to our program:
+## 1. prevent the user from specifying a negative number
+## 2. if the user specifies a positive 'real' number,
+##    we should tell them what value is actually being used
+##
+## 'if' block will help us to check
+## two functions to print are print and stop
+## help(is.integer) - a function that will check if something is an int
